48fa87f728b12b0cda91dc5309646c3b055b74d9
zh92 <zhehao92@gmail.com>
2023-05-12 09:28:01 +0800

    Complete initialization

---
+++ archive_backend/src/main/java/ibf2022/batch2/csf/backend/config/AppConfig.java
@@ -0,0 +1,40 @@
+package ibf2022.batch2.csf.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class AppConfig {
+   @Value("${DO_STORAGE_KEY}")
+   private String accessKey; 
+
+   @Value("${DO_STORAGE_SECRETKEY}")
+   private String secretKey; 
+
+   @Value("${DO_STORAGE_ENDPOINT}")
+   private String endPoint; 
+
+   @Value("${DO_STORAGE_ENDPOINT_REGION}")
+   private String endPointRegion;
+   
+   @Bean
+   public AmazonS3 createS3Client(){
+        BasicAWSCredentials cred =
+                new BasicAWSCredentials(accessKey, secretKey);
+        EndpointConfiguration ep = 
+                new EndpointConfiguration(endPoint, endPointRegion);
+
+        return AmazonS3ClientBuilder.standard()
+            .withEndpointConfiguration(ep)
+            .withCredentials(new AWSStaticCredentialsProvider(cred))
+            .build();
+   }
+}
+
